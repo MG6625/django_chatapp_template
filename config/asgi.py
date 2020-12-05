50cd7a650b5a05665211f3d204b51a61ffebb639
nadure <goglkms@naver.com>
2020-12-06 07:53:09 +0900

    added envar

--- config/asgi.py
+++ config/asgi.py
@@ -1,13 +1,11 @@
-# mysite/asgi.py
 import os
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import chatapp.routing
-os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
 
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
